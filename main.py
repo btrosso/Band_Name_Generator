90299ac0280337f651028eebd88b9f9666b18105
btrosso <51491995+btrosso@users.noreply.github.com>
2022-01-31 15:53:01 -0600

    Create main.py
    Day 1 Project (Level - Beginner)

---
+++ main.py
@@ -0,0 +1,13 @@
+#1. Create a greeting for your program.
+print("Hey! This is my Day 1 Project!\nThis is my Band Name Generator!\n\n")
+
+#2. Ask the user for the city that they grew up in.
+city = input("What city did you grow up in?\n")
+
+#3. Ask the user for the name of a pet.
+petName = input("What is the name of a/your pet?\n")
+
+#4. Combine the name of their city and pet and show them their band name.
+print("Your band's name could be '" + city + " " + petName + "'!")
+
+#5. Make sure the input cursor shows on a new line
